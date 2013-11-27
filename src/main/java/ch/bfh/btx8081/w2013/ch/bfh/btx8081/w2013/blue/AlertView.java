eadbac498aef7659f1dd96438b0c34486455fa60
AdrianWyss <AdrianWyss@dhcp-147-87-42-99.bfh.ch>
2013-11-27 18:41:57 +0100

    test

--- src/main/java/ch/bfh/btx8081/w2013/ch/bfh/btx8081/w2013/blue/AlertView.java
+++ src/main/java/ch/bfh/btx8081/w2013/ch/bfh/btx8081/w2013/blue/AlertView.java
@@ -1,10 +1,15 @@
 package ch.bfh.btx8081.w2013.ch.bfh.btx8081.w2013.blue;
 
 import java.awt.Dimension;
+import java.awt.Rectangle;
 
 import com.vaadin.ui.UI;
 
 public class AlertView {
+	
+	public static Rectangle ivan;
+	
+	
 	//Kommentar
 	public AlertView(){
 		
