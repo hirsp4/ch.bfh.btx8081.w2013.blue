66187bf9093dc346826562ed3d1ab0ba4960cfc2
AdrianWyss <AdrianWyss@dhcp-147-87-42-29.bfh.ch>
2013-12-11 16:18:45 +0100

    state pattern (inkl. javadoc & comment)

--- src/main/java/ch/bfh/btx8081/w2013/ch/bfh/btx8081/w2013/blue/statedesign/Dangerous.java
+++ src/main/java/ch/bfh/btx8081/w2013/ch/bfh/btx8081/w2013/blue/statedesign/Dangerous.java
@@ -7,21 +7,42 @@ import ch.bfh.btx8081.w2013.ch.bfh.btx8081.w2013.blue.database.FileIsEmptyExcept
 import ch.bfh.btx8081.w2013.ch.bfh.btx8081.w2013.blue.database.PersonNotFoundException;
 import ch.bfh.btx8081.w2013.ch.bfh.btx8081.w2013.blue.person.Patient;
 
+/**
+ * 
+ * @author Adrian Wyss
+ * 
+ *         this class extends the basic abstract class states.
+ * 
+ */
+
 public class Dangerous extends State {
 
+	/**
+	 * 
+	 * Declare a valid Person in order to give the person a state
+	 * 
+	 * @param p
+	 * 
+	 */
 	public Dangerous(Patient p) {
 		super(p);
 		this.stateDescription = "DANGEROUS";
 		// TODO Auto-generated constructor stub
 	}
+
 	
-	public void setNormal(){
+	/**
+	 * 
+	 * This method will change a persons state from dangerous to normal
+	 * 
+	 */
+	public void setNormal() {
 		DatabaseHandler dbh = new DatabaseHandler("Patient");
 		try {
 			dbh.removePatient(this.p);
 			p.setState(new Normal(this.p));
 			dbh.addPatient(this.p);
-			
+
 		} catch (FileIsEmptyException | IOException | PersonNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
