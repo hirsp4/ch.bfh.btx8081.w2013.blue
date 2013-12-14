972750bad3e4639f7578b8fe8433d427f5e09be6
Fresh Prince <hirsp4@bfh.ch>
2013-12-14 14:55:42 +0100

    BorderPanel

---
+++ src/main/java/ch/bfh/btx8081/w2013/ch/bfh/btx8081/w2013/blue/view/index/BorderPanel.java
@@ -0,0 +1,20 @@
+package ch.bfh.btx8081.w2013.ch.bfh.btx8081.w2013.blue.view.index;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.ui.Panel;
+
+
+@Theme("mytheme.scss")
+public class BorderPanel extends Panel{
+	private static final long serialVersionUID = 1L;
+	private static final String WIDTH = "390px";
+	private static final String HEIGHT = "600px";
+
+	public BorderPanel(){
+		addStyleName("v-panel-borderpanel");
+		this.setWidth(WIDTH);
+		this.setHeight(HEIGHT);
+	}
+	
+
+}
