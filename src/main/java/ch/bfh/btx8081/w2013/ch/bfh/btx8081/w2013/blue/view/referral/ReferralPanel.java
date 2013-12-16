35ed13484dcd4a6d23d196262279f9db13b279be
keltm1 <keltm1@bfh.ch>
2013-12-16 15:09:14 +0100

    ReferralPanel - Methoden

--- src/main/java/ch/bfh/btx8081/w2013/ch/bfh/btx8081/w2013/blue/view/referral/ReferralPanel.java
+++ src/main/java/ch/bfh/btx8081/w2013/ch/bfh/btx8081/w2013/blue/view/referral/ReferralPanel.java
@@ -22,13 +22,14 @@ import com.vaadin.ui.OptionGroup;
 import com.vaadin.ui.PopupDateField;
 import com.vaadin.ui.TextArea;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.themes.ChameleonTheme;
 
 /**
  * 
  * @author Maja Kelterborn
  * 
  * 
- *         TextFieldPanel v1.0 07.12.2013
+ *         ReferralPanel v2.0  16.12.2013
  * 
  *         Creates the panel for our ReferralView. The panel consists of several
  *         text fields and dropdown to add referral informations, a button to send the referral
@@ -40,6 +41,7 @@ import com.vaadin.ui.Button.ClickEvent;
 public class ReferralPanel extends BorderPanel {
 
 	private static final long serialVersionUID = 1L;
+	private Button SendReferralButton;
 	private ComboBox referralComboBox;
 	private OptionGroup referralTypeGroup;
 	private PopupDateField dateField;
@@ -56,95 +58,50 @@ public class ReferralPanel extends BorderPanel {
 	private Label lab5;
 	
 	public ReferralPanel() {
-		this.referralComboBox = new ComboBox();
-		this.referralComboBox.addItem("Stationery Therapy");
-		this.referralComboBox.addItem("Diagnoses");
-		this.dateField = new PopupDateField("From:");
-		this.dateField.setInputPrompt("Selcet a date");
-		this.dateField.setDateFormat("dd.MM.yyyy");
-		this.dateField.setRangeStart(new Date());
-		this.dateField.setWidth("150px");
-		this.dateField2 = new PopupDateField("To:");
-		this.dateField2.setInputPrompt("Select a date");
-		this.dateField2.setDateFormat("dd.MM.yyyy");
-		this.dateField2.setRangeStart(new Date());
-		this.dateField2.setWidth("150px");
-		this.referralTypeGroup = new OptionGroup();
-		referralTypeGroup.addItem("FU");
-		referralTypeGroup.addItem("Normal");
+		this.addStyleName(ChameleonTheme.PANEL_BORDERLESS);
+		this.setWidth("390px");
+		this.setHeight("600px");
 		this.lab1 = new Label("Reason for referral:");
 		this.lab2 = new Label("Possible Timeframe:");
 		this.lab3 = new Label("Referral type:");
 		this.lab4 = new Label("Choose referral Clinic or Doctor Office:");
 		this.lab5 = new Label("Choose responsible Doctor:");
-		this.textReferralMessage = new TextArea();
-		this.textReferralMessage.setInputPrompt("Additional Notes");
 		this.infoArea = new TextArea();
-		this.clinicComboBox = new ComboBox("Clinic:");
-		this.clinicComboBox.addItem("Psychiatrische Klinik Bern");
-		this.clinicComboBox.addItem("Psychiatrische Klink Basel");
-		this.clinicComboBox.setWidth("150px");
-		this.doctorofficeComboBox = new ComboBox("Doctor Office:");
-		this.doctorofficeComboBox.addItem("Doctor Office - Dr. Rolf Meyer, Basel");
-		this.doctorofficeComboBox.addItem("Doctor Office - Group Office Blue");
-		this.doctorofficeComboBox.addItem("Doctor Office - Group Office Red");
-		this.doctorofficeComboBox.setWidth("150px");
-		this.doctorComboBox = new ComboBox();
-		this.doctorComboBox.addItem("Dr. Helen Fischer");
-		this.doctorComboBox.addItem("Dr. Markus Vetsch");
-		this.doctorComboBox.addItem("Dr. Rolf Meyer");
 		
-		
+
 		FormLayout content = new FormLayout();
 		HorizontalLayout hlayout = new HorizontalLayout();
 		HorizontalLayout hlayout2 = new HorizontalLayout();
 		HorizontalLayout hlayout3 = new HorizontalLayout();
-		content.setSizeUndefined();
-		content.setMargin(true);
-		content.setSpacing(true);
 		content.addComponent(lab1);
-		content.addComponent(this.referralComboBox);
+		content.addComponent(this.createReferralComboBox());
 		content.addComponent(this.lab2);
 		hlayout.setWidth("360px");
-		hlayout.addComponent(this.dateField);
-		hlayout.addComponent(this.dateField2);
+		hlayout.addComponent(this.createDateField1());
+		hlayout.addComponent(this.createDateField2());
 		content.addComponent(hlayout);
 		content.addComponent(this.lab3);
-		content.addComponent(this.referralTypeGroup);
-		content.addComponent(this.textReferralMessage);
+		content.addComponent(this.createOptionGroup());
+		content.addComponent(this.createMessageTextArea());
 		content.addComponent(lab4);
 		hlayout2.setWidth("360px");
-		hlayout2.addComponent(this.clinicComboBox);
-		hlayout2.addComponent(this.doctorofficeComboBox);
+		hlayout2.addComponent(this.createClinicComboBox());
+		hlayout2.addComponent(this.createDoctorOfficeComboBox());
 		content.addComponent(hlayout2);
 		content.addComponent(this.lab5);
-		content.addComponent(this.doctorComboBox);
+		content.addComponent(this.createDoctorComboBox());
 		hlayout3.setWidth("130px");
 		hlayout3.addComponent(this.createLinkIndexButton());
 		hlayout3.addComponent(this.createSendReferralButton());
 		content.addComponent(hlayout3);
 		content.addComponent(this.infoArea);
+		
+		content.setSizeUndefined();
+		content.setMargin(true);
+		content.setSpacing(true);
 		this.setContent(content);
-		
-		clinicComboBox.setImmediate(true);
-		clinicComboBox.addValueChangeListener(new Property.ValueChangeListener() {
-			private static final long serialVersionUID = 1L;
-			@Override
-			public void valueChange(ValueChangeEvent event) {
-				doctorofficeComboBox.setEnabled(false);
-			}
-		});
-		
-		doctorofficeComboBox.setImmediate(true);
-		doctorofficeComboBox.addValueChangeListener(new Property.ValueChangeListener() {
-			private static final long serialVersionUID = 1L;
-			@Override
-			public void valueChange(ValueChangeEvent event) {
-				clinicComboBox.setEnabled(false);
-			}
-		});
 	}
-
+	
 	
 	/**
 	 * Creates the "Index" Button to go back to the Index-View.
@@ -152,16 +109,17 @@ public class ReferralPanel extends BorderPanel {
 	 * @return Button
 	 */
 	public Button createLinkIndexButton() {
-		Button btn2 = new Button("Index");
-		btn2.addClickListener(new Button.ClickListener() {
+		Button IndexButton = new Button("Index");
+		IndexButton.addClickListener(new Button.ClickListener() {
 			private static final long serialVersionUID =1L;
 			public void buttonClick(ClickEvent event) {
 				MyVaadinUI.setIndexView(new IndexView());
 			}
 		});
-		return btn2;
+		return IndexButton;
 	}
 
+	
 	/**
 	 * Creates the "Send Referral" Button and adds a ClickListener to generate the
 	 * output.
@@ -169,7 +127,7 @@ public class ReferralPanel extends BorderPanel {
 	 * @return Button
 	 */
 	public Button createSendReferralButton() {
-		Button SendReferralButton = new Button("Send referral");
+		SendReferralButton = new Button("Send referral");
 		SendReferralButton.addClickListener(new Button.ClickListener() {
 			private static final long serialVersionUID = 1L;
 			
@@ -179,100 +137,120 @@ public class ReferralPanel extends BorderPanel {
 			// the input is correct, and show the error message in the
 			// infoArea field
 			// if the input is incorrect.
-			if (checkInputValues()) {
-				String referral = getReferral();
-				Date dateFrom = getDateFrom();
-				Date dateTo = getDateTo();
-				String referralType = getReferralType();
-				String referralMessage = getReferralMessage();
-				String clinic = getClinic();
-				String doctoroffice = getDoctorOffice(); 
-				String doctor = getDoctor();
+			if(checkInputValues()){
+				Object referral = referralComboBox.getValue();
+				Date dateFrom = dateField.getValue();
+				Date dateTo = dateField2.getValue();
+				Object referralType = referralTypeGroup.getValue();
+				String referralMessage = textReferralMessage.getValue();
+				Object clinic = clinicComboBox.getValue();
+				Object doctoroffice = doctorofficeComboBox.getValue();
+				Object doctor = doctorComboBox.getValue();
 				new CSVCreator(referral + ";" + dateFrom + ";" + dateTo + ";" + referralType + ";" + 
 				referralMessage + ";" + clinic + ";" + doctoroffice + ";" + doctor +";\n", "referral.txt");
 				infoArea.setValue("the referral message has been sent.");
 				} else {
 					infoArea.setValue("the referral message was not sent.");
 				}
+				
 			}
-			
 		});
 		return SendReferralButton;
 	}
 	
-	/**
-	 * Returns the value of the Referral ComboBox.
-	 * 
-	 * @return String
-	 */
-	private String getReferral() {
-		return (String) this.referralComboBox.getValue();
+	
+	private ComboBox createReferralComboBox(){
+		referralComboBox = new ComboBox();
+		referralComboBox.addItem("Stationery Therapy");
+		referralComboBox.addItem("Diagnoses");
+		referralComboBox.setInvalidAllowed(false);
+		referralComboBox.setNullSelectionAllowed(false);
+		return referralComboBox;
 	}
 
-	/**
-	 * Returns the value of the Date Field From.
-	 * 
-	 * @return Date
-	 */
-	private Date getDateFrom() {
-		return this.dateField.getValue();
-	}
 	
-	/**
-	 * Returns the value of the Date Field To.
-	 * 
-	 * @return Date
-	 */
-	private Date getDateTo() {
-		return this.dateField2.getValue();
-	}
-	
-	/**
-	 * Returns the value of the referralTypeGroup Buttons.
-	 * 
-	 * @return String
-	 */
-	private String getReferralType() {
-		return (String) this.referralTypeGroup.getValue();
-	}
-	
-	/**
-	 * Returns the value of the Text Area Referral Message.
-	 * 
-	 * @return String
-	 */
-	private String getReferralMessage() {
-		return this.textReferralMessage.getValue();
-	}
-	
-	/**
-	 * Returns the value of the Clinic ComboBox.
-	 * 
-	 * @return String
-	 */
-	private String getClinic() {
-		return (String) this.clinicComboBox.getValue();
-	}
-
-	/**
-	 * Returns the value of the Doctoroffice ComboBox.
-	 * 
-	 * @return String
-	 */
-	private String getDoctorOffice() {
-		return (String) this.doctorofficeComboBox.getValue();
-	}
-	
-	/**
-	 * Returns the value of the Doctor ComboBox.
-	 * 
-	 * @return String
-	 */
-	private String getDoctor() {
-		return (String) this.doctorComboBox.getValue();
+	private PopupDateField createDateField1(){
+		dateField = new PopupDateField("From:");
+		dateField.setInputPrompt("Selcet a date");
+		dateField.setDateFormat("dd.MM.yyyy");
+		dateField.setRangeStart(new Date());
+		dateField.setWidth("150px");		
+		return dateField;
 	}
 	
 	
+	private PopupDateField createDateField2(){
+		dateField2 = new PopupDateField("To:");
+		dateField2.setInputPrompt("Selcet a date");
+		dateField2.setDateFormat("dd.MM.yyyy");
+		dateField2.setRangeStart(new Date());
+		dateField2.setWidth("150px");
+		return dateField2;
+	}
+	
+	
+	private OptionGroup createOptionGroup(){
+		referralTypeGroup = new OptionGroup();
+		referralTypeGroup.addItem("FU");
+		referralTypeGroup.addItem("Normal");
+		return referralTypeGroup;
+	}
+		
+	
+	private TextArea createMessageTextArea(){
+		textReferralMessage = new TextArea();
+		textReferralMessage.setInputPrompt("Additional Notes");
+		return textReferralMessage;
+	}
+	
+	
+	private ComboBox createClinicComboBox(){
+		clinicComboBox = new ComboBox("Clinic:");
+		clinicComboBox.addItem("Psychiatrische Klinik Bern");
+		clinicComboBox.addItem("Psychiatrische Klink Basel");
+		clinicComboBox.setInvalidAllowed(false);
+		clinicComboBox.setWidth("150px");
+		clinicComboBox.setImmediate(true);
+		clinicComboBox.addValueChangeListener(new Property.ValueChangeListener() {
+			private static final long serialVersionUID = 1L;
+			@Override
+			public void valueChange(ValueChangeEvent event) {
+				doctorofficeComboBox.setEnabled(false);
+				}
+		});
+		return clinicComboBox;	
+	}
+	
+	
+	private ComboBox createDoctorOfficeComboBox() {
+		doctorofficeComboBox = new ComboBox("Doctor Office:");
+		doctorofficeComboBox.addItem("Doctor Office - Dr. Rolf Meyer, Basel");
+		doctorofficeComboBox.addItem("Doctor Office - Group Office Blue");
+		doctorofficeComboBox.addItem("Doctor Office - Group Office Red");
+		doctorofficeComboBox.setInvalidAllowed(false);
+		doctorofficeComboBox.setWidth("150px");
+		doctorofficeComboBox.setImmediate(true);
+		doctorofficeComboBox.addValueChangeListener(new Property.ValueChangeListener() {
+			private static final long serialVersionUID = 1L;
+			@Override
+			public void valueChange(ValueChangeEvent event) {
+				clinicComboBox.setEnabled(false);
+				}
+		});
+		return doctorofficeComboBox;
+	}	
+	
+	
+	private ComboBox createDoctorComboBox() {
+		doctorComboBox = new ComboBox();
+		doctorComboBox.addItem("Dr. Helen Fischer");
+		doctorComboBox.addItem("Dr. Markus Vetsch");
+		doctorComboBox.addItem("Dr. Rolf Meyer");
+		doctorComboBox.setInvalidAllowed(false);
+		doctorComboBox.setNullSelectionAllowed(false);
+		return doctorComboBox;
+	}
+				
 
 	/**
 	 * This private method returns true if and only if all input is correct. It
@@ -286,6 +264,7 @@ public class ReferralPanel extends BorderPanel {
 		boolean validreferral = false;
 		boolean validdateFrom = false;
 		boolean validdateTo = false;
+		boolean validreferralType = false;
 		boolean validreferralMessage = false;
 		boolean validclinicdoctoroffice = false;
 		boolean validdoctor = false;
@@ -294,7 +273,7 @@ public class ReferralPanel extends BorderPanel {
 		// check whether the Referral field is filled. if yes,
 		// the boolean valid referral is set true. if not, a notification
 		// is shown.
-		if (getReferral() == null)  {
+		if (referralComboBox.getValue() == null)  {
 			Notification notif = new Notification("Input failure", 
 					"Select one reason for referral from the dropdown menu.",
 					Notification.Type.WARNING_MESSAGE);
@@ -309,30 +288,38 @@ public class ReferralPanel extends BorderPanel {
 				// and if the  Date From is before the Date To.
 				// if yes, the boolean valid referral is set true. 
 				// if not, a notification is shown.
-		if (getDateFrom() == null && getDateTo() == null) {
+		if (dateField.getValue() == null && dateField2.getValue() == null) {
 			Notification notif = new Notification("Input failure",
 					"Please select a date",
 					Notification.Type.WARNING_MESSAGE);
         	notif.setDelayMsec(5000);
         	notif.setPosition(Position.BOTTOM_RIGHT);
 			notif.show(Page.getCurrent());}
-		else if (!getDateFrom().before(getDateTo())) {
+		else if (!dateField.getValue().before(dateField2.getValue())) {
 			Notification notif = new Notification("Input failure",
 					"Date From has to be before Date To",
 					Notification.Type.WARNING_MESSAGE);
         	notif.setDelayMsec(5000);
         	notif.setPosition(Position.BOTTOM_RIGHT);
 			notif.show(Page.getCurrent());}	
-		 else {
+		 else 
 			validdateFrom = true;
 			validdateTo = true;
-		}
-		
-		
+
+			if(referralTypeGroup.getValue() == null) {
+				Notification notif = new Notification("Input failure", 
+						"Please select one referral type.",
+						Notification.Type.WARNING_MESSAGE);
+	        	notif.setDelayMsec(5000);
+	        	notif.setPosition(Position.BOTTOM_RIGHT);
+				notif.show(Page.getCurrent());
+			} else 
+				validreferralType = true;
+			
 		// check whether the Referral Message field is filled. if yes,
 				// the boolean valid referral is set true. if not, a notification
 				// is shown.
-		if (getReferralMessage().isEmpty())  {
+		if (textReferralMessage.getValue().isEmpty())  {
 			Notification notif = new Notification("Input failure", "Text fields can't be empty.",
 					Notification.Type.WARNING_MESSAGE);
         	notif.setDelayMsec(5000);
@@ -345,7 +332,7 @@ public class ReferralPanel extends BorderPanel {
 		// check whether the DoctorOffice field is filled. if yes,
 				// the boolean valid referral is set true. if not, a notification
 				// is shown.
-		if (getDoctorOffice() == null && getClinic() == null)  {
+		if (doctorofficeComboBox.getValue() == null && clinicComboBox.getValue() == null)  {
 			Notification notif = new Notification("Input failure", 
 					"Select a Clinic or a Doctor Office from the dropdown menu.",
 					Notification.Type.WARNING_MESSAGE);
@@ -359,7 +346,7 @@ public class ReferralPanel extends BorderPanel {
 		// check whether the Doctor field is filled. if yes,
 				// the boolean valid referral is set true. if not, a notification
 				// is shown.
-		if (getDoctor() == null)  {
+		if (doctorComboBox.getValue() == null)  {
 			Notification notif = new Notification("Input failure", 
 					"Select a Doctor from the dropdown menu.",
 					Notification.Type.WARNING_MESSAGE);
@@ -369,9 +356,13 @@ public class ReferralPanel extends BorderPanel {
 		} else
 			validdoctor = true;
 
+		
 		// returns true, if and only if both booleans are true.
-		return validreferral && validdateFrom && validdateTo && validreferralMessage
+		return  validreferral && validdateFrom && validdateTo && validreferralType && validreferralMessage
 				&& validclinicdoctoroffice && validdoctor;
 	}
 }
+
+
+		
 
